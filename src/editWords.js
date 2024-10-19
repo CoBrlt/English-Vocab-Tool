36317a37493109f286be014a1623e48783eef2b6
unknown <corentin.berlot123@gmail.com>
2024-10-19 11:22:23 +0200

    front

--- src/editWords.js
+++ src/editWords.js
@@ -1,5 +1,6 @@
 const { ipcRenderer } = require('electron');
-const List = require("./List.js")
+const List = require("./List.js");
+const Example = require('./Example.js');
 var currentList = null;
 
 
@@ -14,9 +15,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
         currentList = new List()
         currentList.fromJson(data)
         console.log(currentList)
+        handleLineAdd()
         createAllLines()
     });
 })
+
+function handleLineAdd(){
+    let btnAddWord = document.getElementById("btn-add-word")
+    btnAddWord.addEventListener("click", ()=>{
+        let tr = document.getElementById("tr-add-word")
+        let word = htmlTrToObjectWord(tr)
+        // askServerToAdd
+    })
+}
 
 function createAllLines(){
     let listWords = currentList.getWords()
@@ -29,23 +40,48 @@ function createAllLines(){
 
 function createLine(word){
     let newTr = document.createElement("tr")
+    
+    let tdDelete = document.createElement("td")
+    let buttonDelete = document.createElement("button")
+    buttonDelete.className = "button"
+    buttonDelete.textContent = "Delete"
+    buttonDelete.addEventListener("click", ()=>{
+        let word = null
+        if(askServerToRemoveWord(word)){
+            newTr.remove()
+        }
+    })
+    tdDelete.append(buttonDelete)
 
-    let tdEnglishField = createEnglishField(word.getEnglish())
-    let tdTranslationField = createTranslationField(word.getFrench())
-    let tdExamplesFiled = createExamplesField(word.getExamples())
 
     let tdSave = document.createElement("td")
     let buttonSave = document.createElement("button")
     buttonSave.className = "button"
     buttonSave.textContent = "Save"
     buttonSave.style.display = "none"
+    buttonSave.addEventListener("click", ()=>{
+        let word = htmlTrToObjectWord(newTr)
+        if(askServerToSaveWord(word)){
+            buttonSave.style.display = "none"
+            buttonDelete.style.display = "inline-block"
+        }
+    })
     tdSave.append(buttonSave)
 
-    let tdDelete = document.createElement("td")
-    let buttonDelete = document.createElement("button")
-    buttonDelete.className = "button"
-    buttonDelete.textContent = "Delete"
-    tdDelete.append(buttonDelete)
+    
+    let showSaveAndHideDelete = (state)=>{
+        if(state){
+            buttonDelete.style.display = "none"
+            buttonSave.style.display = "inline-block"
+        }else{
+            buttonDelete.style.display = "inline-block"
+            buttonSave.style.display = "none"
+        }
+    }
+
+    let tdEnglishField = createEnglishField(word.getEnglish(), showSaveAndHideDelete)
+    let tdTranslationField = createTranslationField(word.getFrench(), showSaveAndHideDelete)
+    let tdExamplesFiled = createExamplesField(word.getExamples(), showSaveAndHideDelete)
 
     newTr.appendChild(tdEnglishField)
     newTr.appendChild(tdTranslationField)
@@ -56,7 +92,7 @@ function createLine(word){
     return newTr
 }
 
-function createEnglishField(englishWord){
+function createEnglishField(englishWord, showSaveAndHideDelete){
     let td = document.createElement("td")
     td.className = "english"
     
@@ -65,76 +101,129 @@ function createEnglishField(englishWord){
     input.className = "editable"
     input.placeholder = "Enter English word"
     input.value = englishWord
+    input.addEventListener("input", ()=>{
+        showSaveAndHideDelete(true)
+    })
 
     td.append(input)
     return td
 }
 
-function createTranslationField(translationsList){
+function createTranslationField(translationsList, showSaveAndHideDelete){
     let td = document.createElement("td")
     td.className = "translations"
 
     for(let translation of translationsList){
-        let div = document.createElement("div")
-        div.className = "translation-item"
-
-        let input = document.createElement("input")
-        input.type = "text"
-        input.className = "editable"
-        input.value = translation
-
-        let buttonRemove = document.createElement("button")
-        buttonRemove.className = "btn-remove"
-        buttonRemove.textContent = "×"
-    
-        div.appendChild(input)
-        div.appendChild(buttonRemove)
+        let div = createInputTranslation(showSaveAndHideDelete, translation)
         td.appendChild(div)
     }
 
     let buttonAdd = document.createElement("button")
     buttonAdd.className = "btn-add"
     buttonAdd.textContent = "Add translation"
-    
+    buttonAdd.addEventListener("click", ()=>{
+        let div = createInputTranslation(showSaveAndHideDelete, "", "Enter translation")
+        td.insertBefore(div, buttonAdd)
+        showSaveAndHideDelete(true)
+    })
     td.appendChild(buttonAdd)
+    
     return td
 }
 
-
-function createExamplesField(examplesList){
+function createExamplesField(examplesList, showSaveAndHideDelete){
     let td = document.createElement("td")
     td.className = "examples"
 
     for(let example of examplesList){
-        let div = document.createElement("div")
-        div.className = "example-item"
-
-        let createTextArea = (content)=>{
-            let textArea = document.createElement("textarea")
-            textArea.className = "editable"
-            textArea.textContent = content
-            return textArea
-        }  
-
-        let englishTextArea = createTextArea(example.getEnglish())
-        let frenchTextArea = createTextArea(example.getFrench())               
-
-        let buttonRemove = document.createElement("button")
-        buttonRemove.className = "btn-remove"
-        buttonRemove.textContent = "×"
-    
-        div.appendChild(englishTextArea)
-        div.appendChild(frenchTextArea)
-        div.appendChild(buttonRemove)
+        let div = createInputExample(showSaveAndHideDelete, example)
         td.appendChild(div)
     }
 
     let buttonAdd = document.createElement("button")
     buttonAdd.className = "btn-add"
     buttonAdd.textContent = "Add example"
+    buttonAdd.addEventListener("click", ()=>{
+        let div = createInputExample(showSaveAndHideDelete, new Example("", ""))
+        td.insertBefore(div, buttonAdd)
+        showSaveAndHideDelete(true)
+    })
     
     td.appendChild(buttonAdd)
     return td
+}
+
+function createInputExample(showSaveAndHideDelete, example=null){
+    let div = document.createElement("div")
+    div.className = "example-item"
+
+    let createTextArea = (content, placeholder="")=>{
+        let textArea = document.createElement("textarea")
+        textArea.className = "editable"
+        textArea.textContent = content
+        textArea.placeholder = placeholder
+        textArea.addEventListener("input", ()=>{
+            showSaveAndHideDelete(true)
+        })
+        return textArea
+    }  
+    
+    let englishTextArea = createTextArea(example.getEnglish(), "Enter English Exemple")
+    let frenchTextArea = createTextArea(example.getFrench(), "Enter example translation")
+
+    let buttonRemove = document.createElement("button")
+    buttonRemove.className = "btn-remove"
+    buttonRemove.textContent = "×"
+    buttonRemove.addEventListener("click", ()=>{
+        div.remove()
+        showSaveAndHideDelete(true)
+    })
+
+    div.appendChild(englishTextArea)
+    div.appendChild(frenchTextArea)
+    div.appendChild(buttonRemove)
+    return div
+}
+
+
+function createInputTranslation(showSaveAndHideDelete, translation="", placeholerContent=""){
+    let div = document.createElement("div")
+    div.className = "translation-item"
+
+    let input = document.createElement("input")
+    input.type = "text"
+    input.className = "editable"
+    input.value = translation
+    input.placeholder = placeholerContent
+    input.addEventListener("input", ()=>{
+        showSaveAndHideDelete(true)
+    })
+
+    let buttonRemove = document.createElement("button")
+    buttonRemove.className = "btn-remove"
+    buttonRemove.textContent = "×"
+    buttonRemove.addEventListener("click", ()=>{
+        div.remove()
+        showSaveAndHideDelete(true)
+    })
+
+    div.appendChild(input)
+    div.appendChild(buttonRemove)
+    return div
+}
+
+
+function htmlTrToObjectWord(tr){
+    return null
+}
+
+
+function askServerToSaveWord(word){
+    return true
+}
+
+function askServerToRemoveWord(word){
+    return true
 }
 
 
